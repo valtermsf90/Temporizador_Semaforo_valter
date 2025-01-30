93d951d70fb5db647dd1b75278465a67e38a632a
machadof90 <machadofilho.ti@gmail.com>
2025-01-29 22:55:44 -0300

    Atualiza mensagens do semáforo para PARE, ATENÇÃO e SIGA; altera sinal inicial para 1

--- Clock_temporizador.c
+++ Clock_temporizador.c
@@ -2,59 +2,69 @@
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
-#define LED_G 11 //pino led verde
-#define LED_R 13 //pino led vermelho
+#define LED_G 11 // pino led verde
+
+#define LED_R 13 // pino led vermelho
 
 bool status = true;
-int sinal = 0;
+int sinal = 1;
 
-bool semaforo(struct repeating_timer *t) {
-   
-   if(sinal == 3){
+// semaforo
+bool semaforo(struct repeating_timer *t)
+{
+    // troca sinal
+    if (sinal == 3)
+    {
         sinal = 1;
-    }else{
+    }
+    else
+    {
         sinal++;
     }
-    switch (sinal)
+    switch (sinal) // verifica sinal
     {
-    case 1:
+    case 1: // vermelho
         gpio_put(LED_G, !status);
         gpio_put(LED_R, status);
-        printf("vermelho\n");
+        printf("PARE\n");
         break;
-        case 2:
+    case 2: // amarelo
         gpio_put(LED_G, status);
         gpio_put(LED_R, status);
-        printf("amarelo\n");
+        printf("ATENÇÃO\n");
         break;
-        case 3:
+    case 3: // verde
         gpio_put(LED_G, status);
-        gpio_put(LED_R, !status);   
-        printf("verde\n");
-        break;    
+        gpio_put(LED_R, !status);
+        printf("SIGA\n");
+        break;
     default:
         break;
     }
     return true;
 }
 
-
-
-
-
 int main()
 {
+    // inicia comunicacao serial
     stdio_init_all();
 
+    // inicia GPIO
     gpio_init(LED_G);
     gpio_init(LED_R);
     gpio_set_dir(LED_G, GPIO_OUT);
     gpio_set_dir(LED_R, GPIO_OUT);
 
-   struct repeating_timer timer ;
-   add_repeating_timer_ms(3000, semaforo, NULL, &timer);
-   
-    while (true) {
+    // inicia led vermelho ligado
+    gpio_put(LED_R, status);
+
+    // inicia semaforo
+    struct repeating_timer timer;
+    add_repeating_timer_ms(3000, semaforo, NULL, &timer);
+
+    // loop
+    while (true)
+    {
         printf("Respeite a sinalização.\n");
         sleep_ms(1000);
     }
